e79cd941d07874f1e825e14d5b19d976b3d90eed
ishland <ishlandmc@yeah.net>
2023-02-25 12:13:53 +0800

    Sync C2ME CheckedThreadLocalRandom
    RelativityMC/C2ME-fabric#197

--- src/main/java/com/ishland/uwrad/TheMixinPlugin.java
+++ src/main/java/com/ishland/uwrad/TheMixinPlugin.java
@@ -1,10 +1,17 @@
 package com.ishland.uwrad;
 
+import com.ishland.uwrad.common.Config;
+import com.ishland.uwrad.common.debug.SMAPPool;
 import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import org.spongepowered.asm.mixin.transformer.ClassInfo;
+import org.spongepowered.asm.mixin.transformer.IMixinTransformer;
+import org.spongepowered.asm.mixin.transformer.ext.Extensions;
+import org.spongepowered.asm.mixin.transformer.ext.IExtension;
+import org.spongepowered.asm.mixin.transformer.ext.ITargetClassContext;
 
 import java.util.List;
 import java.util.Set;
@@ -12,7 +19,33 @@ import java.util.Set;
 public class TheMixinPlugin implements IMixinConfigPlugin {
     @Override
     public void onLoad(String mixinPackage) {
+        Config.init();
+        if (MixinEnvironment.getCurrentEnvironment().getActiveTransformer() instanceof IMixinTransformer transformer &&
+            transformer.getExtensions() instanceof Extensions extensions) {
+            extensions.add(new IExtension() {
+                @Override
+                public boolean checkActive(MixinEnvironment environment) {
+                    return true;
+                }
 
+                @Override
+                public void preApply(ITargetClassContext context) {
+
+                }
+
+                @Override
+                public void postApply(ITargetClassContext context) {
+
+                }
+
+                @Override
+                public void export(MixinEnvironment env, String name, boolean force, ClassNode classNode) {
+                    SMAPPool.put(name, classNode);
+                }
+            });
+        } else {
+            System.err.println("Failed to initialize SMAP parser for safe world random access, mod information for mixin injected methods will not be available");
+        }
     }
 
     @Override
---
+++ src/main/java/com/ishland/uwrad/common/Config.java
@@ -0,0 +1,68 @@
+package com.ishland.uwrad.common;
+
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
+public class Config {
+
+    public static final boolean ENFORCE_SAFE_WORLD_RANDOM_ACCESS;
+
+    static {
+        final Properties properties = new Properties();
+        final Properties newProperties = new Properties();
+        final Path path = FabricLoader.getInstance().getConfigDir().resolve("uwrad.properties");
+        if (Files.isRegularFile(path)) {
+            try (InputStream in = Files.newInputStream(path, StandardOpenOption.CREATE)) {
+                properties.load(in);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        ENFORCE_SAFE_WORLD_RANDOM_ACCESS = getBoolean(properties, newProperties, "enforce_safe_world_random_access", true);
+        try (OutputStream out = Files.newOutputStream(path, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+            newProperties.store(out, "Configuration file for unsafe-world-random-access-detector");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void init() {
+    }
+
+    private static int getInt(Properties properties, Properties newProperties, String key, int def) {
+        try {
+            final int i = Integer.parseInt(properties.getProperty(key));
+            newProperties.setProperty(key, String.valueOf(i));
+            return i;
+        } catch (NumberFormatException e) {
+            newProperties.setProperty(key, String.valueOf(def));
+            return def;
+        }
+    }
+
+    private static boolean getBoolean(Properties properties, Properties newProperties, String key, boolean def) {
+        try {
+            final boolean b = parseBoolean(properties.getProperty(key));
+            newProperties.setProperty(key, String.valueOf(b));
+            return b;
+        } catch (NumberFormatException e) {
+            newProperties.setProperty(key, String.valueOf(def));
+            return def;
+        }
+    }
+
+    private static boolean parseBoolean(String string) {
+        if (string == null) throw new NumberFormatException("null");
+        if (string.trim().equalsIgnoreCase("true")) return true;
+        if (string.trim().equalsIgnoreCase("false")) return false;
+        throw new NumberFormatException(string);
+    }
+
+}
